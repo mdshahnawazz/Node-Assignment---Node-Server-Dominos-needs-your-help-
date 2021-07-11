bba6e8a162ede8b69d93bebd1725874a14fe44bd
mdshahnawazz <70908310+mdshahnawazz@users.noreply.github.com>
2021-07-11 18:36:11 +0530

    first first

--- src/index.js
+++ src/index.js
@@ -4,7 +4,28 @@ const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
-  
+  const { json } = require("express");
+var http = require("http");
+const httpServer = http.createServer(handleServer);
+function handleServer(req, res) {
+    if (req.url === '/welcome') {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/plain");
+        res.end("Welcome to Dominos!");
+    } else if (req.url === '/contact') {
+        const obj = {
+            phone: "18602100000",
+            email: "guestcaredominos@jublfood.com"
+        };
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/json");
+        res.end(JSON.stringify(obj));
+    } else {
+        res.writeHead(404).end('page not found');
+    }
 }
-
+//Create a Node Server that listens to PORT 8081.
+httpServer.listen('8081', () => {
+    console.log('Listening');
+})
 module.exports = httpServer;
